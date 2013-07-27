3cb7923734b3003c682ca170a46c5e3fcf756ffc
Marvin L. Kassabian <mlkassab@mtu.edu>
2013-07-27 02:13:58 -0400

    Understood the Magic of limeJS

--- gamejambeta.js
+++ gamejambeta.js
@@ -1,7 +1,6 @@
 //set main namespace
 goog.provide('gamejambeta');
 
-
 //get requirements
 goog.require('lime.Director');
 goog.require('lime.Scene');
@@ -12,34 +11,36 @@ goog.require('lime.animation.Spawn');
 goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
+goog.require('gamejambeta.NaturalDisasterQueue');
+goog.require('gamejambeta.Colony');
+goog.require('gamejambeta.UserInterface');
 
 
 // entrypoint
 gamejambeta.start = function(){
 
     var gameObj = {
-        tile_size: 64,
-        num_tiles_x: 20,
-        num_tiles_y: 16,
-        width: num_tiles_x*tile_size,   //1280
-        height: num_tiles_y*tile_size,  //1024
-        userInterfaceLayer_w: tile_size*5, 
-        userInterfaceLayer_h: tile_size*6
+        tile_size: 16,
+        num_tiles_x: 80,
+        num_tiles_y: 64,
+        width: 1280,
+        height: 1024,
+        userInterfaceLayer_w: 1280, 
+        userInterfaceLayer_h: 160
     }
 
-	var director = new lime.Director(document.body,1024,768),
+	var director = new lime.Director(document.body,gameObj.width,gameObj.height),
 	    scene = new lime.Scene(),
         naturalDisasterQueue = new gamejambeta.NaturalDisasterQueue(gameObj),
-        colony = new gamejambeta.colony(gameObj)
+        colony = new gamejambeta.Colony(gameObj)
         userInterface = new gamejambeta.UserInterface(gameObj);
+
+    scene.appendChild(userInterface);
 
 	director.makeMobileWebAppCapable();
 
-    //add some interaction
-
     lime.scheduleManager.schedule(function(dt) {
-        gamejambeta.NaturalDisaster.scheduleShit(dt);
-
+        //gamejambeta.NaturalDisaster.scheduleShit(dt);
     },this);
 
 
@@ -48,6 +49,11 @@ gamejambeta.start = function(){
 
 }
 
+
+/* Case 1: Falling Shit
+ * Case 2: Aliens
+ * Case 3: Electric Storm
+ */
 
 //this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
 goog.exportSymbol('gamejambeta.start', gamejambeta.start);
--- userinterface.js
+++ userinterface.js
@@ -1,8 +1,23 @@
 goog.provide('gamejambeta.UserInterface');
-goog.require('lime.Sprite');
 
-gamejambeta.UserInterface = function(gameObj, playerObj) {
+goog.require('lime.Layer');
+
+gamejambeta.UserInterface = function(gameObj) {
 	goog.base(this);
+	this.setAnchorPoint(0,0);
+	
+	var uiBackground = new lime.Sprite()
+		.setSize(gameObj.tile_size*gameObj.userInterfaceLayer_w,
+			gameObj.tile_size*gameObj.userInterfaceLayer_h)
+        .setFill('#c00')
+        .setAnchorPoint(0,0)
+        .setPosition(gameObj.tile_size*0,
+			gameObj.tile_size*54);
+
+	this.setSize(gameObj.tile_size*gameObj.userInterfaceLayer_w,
+		gameObj.tile_size*gameObj.userInterfaceLayer_h);
+
+	this.appendChild(uiBackground);
 }
 
-goog.inherits(gamejambeta.UserInterface,lime.Sprite);
+goog.inherits(gamejambeta.UserInterface,lime.Layer);
